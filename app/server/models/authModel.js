06ee17977a877e9e36d3d60cbd84217c09d68e96
Khadja <khadjapaux.contact@gmail.com>
2023-01-23 20:19:33 +0100

    need to new requirement while signing up

--- app/server/models/authModel.js
+++ app/server/models/authModel.js
@@ -1,8 +1,12 @@
-const require mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
+    username:{
+
+    }
     email: {
         type: String,
         require: true,
@@ -10,9 +14,27 @@ const userSchema = new Schema({
     },
     password:{
         type:String,
-        required: true
-    }, 
+        required: true,
+    }
+    biography:{
+        
+    }
+});
 
-})
+//static signup method on AuthModel
+userSchema.statics.signup = async (email, password) => {
+    //check if the email exist on the db 
+    const exists = await this.findOne({ email }); //(this refers to userSchema) 
+    //
+    const salt = await bcrypt.genSalt(10);
+    const hash = await bcrypt.hash(password, salt);
 
-module.exports = mongoose.model('Auth', authSchema);
+    const user = await this.create({email, password : hash})
+    if(exists){
+        throw Error('Email already used, please enter another adress'); 
+    }
+    
+    return user
+
+}
+module.exports = mongoose.model('User', userSchema);
