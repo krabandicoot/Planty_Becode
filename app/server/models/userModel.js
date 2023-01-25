4bd7fe3bac16edcc41874b9d86a246d9304f9c30
Khadja <khadjapaux.contact@gmail.com>
2023-01-25 10:58:43 +0100

    error signup function

---
+++ app/server/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
+const Schema = mongoose.Schema;
+
+const userSchema = new Schema({
+    email:{
+        type:String,
+        required: true,
+        unique: true,
+    },
+    password:{
+        type:String,
+        required:true,
+    }
+});
+
+userSchema.static.signup = async(email, password) =>{
+
+    const exists = await this.findOne({ email });
+    if(exists){
+        throw Error('Email already used, please enter another adress');
+    }
+    const salt = await bcrypt.genSalt(10);//a random string of character that get added to the user password to prevent getting hacked
+    const hash = await bcrypt.hash(password, salt);
+
+    const user = await this.create({email, password: hash });
+
+    return user;
+}
+module.exports = mongoose.model('User', userSchema);
--- app/server/server.js
+++ app/server/server.js
@@ -2,32 +2,25 @@ require ('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
 
-// --------- import routes
-const authRoutes = require('./routes/authRoute');
+const userRoutes = require('./routes/user');
 
-//transter Express content in a variable called app
 const app = express();
 const PORT = process.env.NODE_DOCKER_PORT;
-//storing the URL in a variable MongoString
 const mongoString = process.env.DB_URL;
 
 // --------- connect to DB 
-//connection the database to our server with mongoose
 mongoose.connect(mongoString);
 const database = mongoose.connection;
 
-//throw a sucess or error message on the database connection 
 database.on('error', (error) => {
     console.log(`Database couldn't connect properly :\n ${error}`)
 });
 database.once('connected', () => {
     console.log('Database Connected 📬');
 });
-// --------- middleware
-app.use(express.json());
-// --------- Routes
-app.use('/api/user', authRoutes);
 
+app.use(express.json());
+app.use('/api/user', userRoutes);
 app.get("/", (req, res) => {
     res.json("Welcome to planty application ☘️");
 });
