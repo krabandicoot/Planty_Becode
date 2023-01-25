d4e62af815c6eb3538fae44a45e5ead55408a8ba
TreshMiralissa <muratet.jade@yahoo.com>
2023-01-25 16:46:05 +0100

    essai 3 title

--- app/client/src/App.jsx
+++ app/client/src/App.jsx
@@ -1,6 +1,4 @@
 import "./App.css";
-import Lottie from "lottie-react";
-import animationData from './lotties/logo-Planty.json';
 import { LandinPage } from "./components/LandingPage";
 import { Title } from "./components/Title";
 
