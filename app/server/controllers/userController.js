a291de85abf8171728e77421c6f4e0771fbf42ff
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2023-01-26 16:15:41 +0100

    adding tokken to package

--- app/server/controllers/userController.js
+++ app/server/controllers/userController.js
@@ -1,40 +1,40 @@
 const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
-require ('dotenv').config();
+require('dotenv').config();
 
 const TOKEN = process.env.NODE_TOKEN;
 
 //function to generate a token in the signin and signup user 
-const createToken = (_id )=>{//id =  is part of the payload of the token 
-    return jwt.sign({_id},TOKEN, {expiresIn : '3d'});//the second is the secret string only know by the server, the third means the user stays logged in for 3 days before the token expires
+const createToken = (_id) => {//id =  is part of the payload of the token 
+    return jwt.sign({ _id }, TOKEN, { expiresIn: '3d' });//the second is the secret string only know by the server, the third means the user stays logged in for 3 days before the token expires
 }
 
 //login user 
-const signInUser = async(req, res) => {
-    const {username, password} = req.body;
-    
-    try{
+const signInUser = async (req, res) => {
+    const { username, password } = req.body;
+
+    try {
         const user = await User.signin(username, password);
 
         //create token
-        const signInToken = createToken(user._id); 
-        res.status(200).json({username, signInToken});
-    }catch(error){
-        res.status(400).json({error: error.message});
+        const signInToken = createToken(user._id);
+        res.status(200).json({ username, signInToken });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
 }
 //signup user 
-const signUpUser = async(req, res) => {
-    const {username,email, password,color} = req.body;
+const signUpUser = async (req, res) => {
+    const { username, email, password, color } = req.body;
 
-    try{
-        const user = await User.signup(username,email, password, color);
+    try {
+        const user = await User.signup(username, email, password, color);
 
         //create token
-        const signInToken = createToken(user._id); 
-        res.status(200).json({email, signInToken});
-    }catch(error){
-        res.status(400).json({error: error.message});
+        const signInToken = createToken(user._id);
+        res.status(200).json({ email, signInToken });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
     }
 }
 
@@ -42,4 +42,4 @@ const signUpUser = async(req, res) => {
 //     const {username, password} = req.body
 // }
 
-module.exports = { signInUser, signUpUser, signOutUser};
+module.exports = { signInUser, signUpUser, signOutUser };
