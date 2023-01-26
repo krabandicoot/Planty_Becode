fd6256a6b7c7210e6c838cd100be0232028b3478
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2023-01-26 11:14:59 +0100

    fixing conflicts after merge w/ khadja

--- app/client/src/components/Login.jsx
+++ app/client/src/components/Login.jsx
@@ -1,17 +1,39 @@
+import { useState } from "react";
+
 export function Login() {
+
+    async function loginUser(credentials) {
+        return fetch('localhost:8080/api/user/signin', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(credentials)
+        })
+            .then(data => data.json())
+    }
+
     return (
-        <form method="post">
-            <label for="username"></label>
-            <input type="text" placeholder="Enter Username" name="username" required />
+        <div className="form__container">
+            <form method="post">
+                <div className="form__username">
+                    <label>Username</label>
+                    <input type="text" placeholder="Enter Username" name="username" required />
 
-            <label for="password"></label>
-            <input type="text" placeholder="Enter Password" name="password" required />
+                </div>
 
-            <button type="submit">Login</button>
+                <div className="form__password">
+                    <label>Password</label>
+                    <input type="password" placeholder="Enter Password" name="password" required />
 
-            <label>
-                <input type="checkbox" checked="checked" name="rememberMe" />
-            </label>
-        </form>
+                </div>
+
+                <button type="submit">Login</button>
+
+                <label>
+                    <input type="checkbox" checked="checked" name="rememberMe" />
+                </label>
+            </form>
+        </div>
     )
 }
