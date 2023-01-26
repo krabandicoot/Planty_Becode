c384ef28f255076edac278bd5aea1b5b66945629
TreshMiralissa <muratet.jade@yahoo.com>
2023-01-26 16:09:35 +0100

    form sign up style

--- app/client/src/components/SignUp.jsx
+++ app/client/src/components/SignUp.jsx
@@ -1,74 +1,98 @@
 import React, {useState} from "react";
+import axios from "axios";
 
-export function SignUp() {
-    const [username, setUsername] = useState("");
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log({ username, email, password });    
-        setUsername("");
-        setEmail("");
-        setPassword("");
+export function SignUp () {
+    const [user,setUser] = useState({
+        username:"",
+        email:"",
+        password: ""
+    })
+    const handleChange = e =>{
+        const {name,value} = e.target
+        setUser({
+        ...user,//spread operator 
+        [name]:value
+        })
     }
-    return (
-        <div className='signup__container'>
-            <h2>Sign up</h2>
-            <form method="POST" className='signup__form' onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor='username'>Username</label>
-                    <input
-                        type='text'
-                        id='username'
-                        name='username'
-                        value={username}
-                        required
-                        placeholder="Enter Username"
-                        onChange={(e) => setUsername(e.target.value)}
-                    />
+//register function 
+    // const register = ()=>{
+    //     const {name,email,password} = user
+    //     if (name && email && password){
+    //         axios.post("http://localhost:6969/Register",user )
+    //         .then(res=>console.log(res))
+    //     }
+    //     else{
+    //         alert("invalid input")
+    //     };
+
+        return (
+
+            <form method="POST" className="signup__form" onSubmit={handleChange}>
+    {/* username */}
+                < div className="signup__form-username relative z-0 w-full mb-6 group">
+                    <input 
+                    type="text" 
+                    name="username" 
+                    id="username" 
+                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+                    placeholder=" " 
+                    required 
+                    value={user.username} 
+                    onChange={handleChange}/>
+                    <label 
+                    htmlFor="username" 
+                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Username</label>
                 </div>
-                <div>
-                    <label htmlFor='email'>Email</label>
-                    <input
-                        type='email'
-                        name='email'
-                        id='email'
-                        value={email}
-                        required
-                        placeholder="Enter Email"
-                        onChange={(e) => setEmail(e.target.value)}
-                    />
+    {/* email */}
+                <div className="signup__form-email relative z-0 w-full mb-6 group">
+                    <input 
+                    type="email" 
+                    name="email" 
+                    id="email" 
+                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+                    placeholder=" " 
+                    required 
+                    value={user.email} 
+                    onChange={handleChange}/>
+                    <label 
+                    htmlFor="email" 
+                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email address</label>
                 </div>
-                <div>
-                    <label htmlFor='password'>Password</label>
-                    <input
-                        type='password'
-                        name='password'
-                        id='password'
-                        minLength={8}
-                        required
-                        value={password}
-                        placeholder="Enter Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+    {/* password */}
+                <div className="signup__form-password relative z-0 w-full mb-6 group">
+                    <input 
+                    type="password" 
+                    name="password" 
+                    id="password" 
+                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+                    placeholder=" " 
+                    required 
+                    minLength={8} 
+                    value={user.password} 
+                    onChange={handleChange} />
+                    <label 
+                    htmlFor="floating_password" 
+                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
                 </div>
-                <div>
-                    <label htmlFor='password'>Password verification</label>
-                    <input
-                        type='password'
-                        name='passwordVerification'
-                        id='passwordVerification'
-                        minLength={8}
-                        required
-                        value={password}
-                        placeholder="Enter Again Password"
-                        onChange={(e) => setPassword(e.target.value)}
-                    />
+    {/* repeat password */}
+                <div className="signup__form-repeat_password relative z-0 w-full mb-6 group">
+                    <input 
+                    type="password" 
+                    name="repeat_password" 
+                    id="repeat_password" 
+                    className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer" 
+                    placeholder=" " 
+                    required 
+                    value={user.password} 
+                    onChange={handleChange}/>
+                    <label 
+                    htmlFor="floating_repeat_password" 
+                    className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Confirm password</label>
                 </div>
-                <button className='signupBtn'>Sign Up</button>
+    {/* button sign up */}
+                <button type="submit" className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Sign Up</button>
             </form>
-        </div>
-    )
+        )
+    // }
 }
+
