857bbdb8e04d11095e8c0c1e6100371f1ed57314
TreshMiralissa <muratet.jade@yahoo.com>
2023-01-27 09:15:37 +0100

    before merge with lisa

--- app/client/src/components/SignUp.jsx
+++ app/client/src/components/SignUp.jsx
@@ -76,7 +76,7 @@ export function SignUp () {
                     <label 
                     htmlFor="floating_password" 
                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 -z-10 origin-[0] peer-focus:left-0 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Choose password</label>
-                    <button onClick={()=> setPasswordVisible(! passwordVisible)}>()</button>
+                    <button onClick={()=> setPasswordVisible(! passwordVisible)}>( )</button>
                 </div>
     {/* confirm password */}
                 <div className="signup__form-repeat_password relative z-0 w-full mb-6 group">
