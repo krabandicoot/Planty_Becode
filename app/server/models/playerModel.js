d9bfb4ada6052c8d2d8e1f51e1f5c5410b41a4a6
Da-nae <grosjean.danae@gmail.com>
2023-01-27 12:29:44 +0100

    test aggregate (not conclusive)

--- app/server/models/playerModel.js
+++ app/server/models/playerModel.js
@@ -5,29 +5,6 @@ const validator = require('validator');
 const Schema = mongoose.Schema;
 
 const playerSchema = new Schema({
-    username:{
-        type: String,
-        minLength: 4,
-        maxLength: 255,
-        required: true,
-        unique: true
-    },
-    email:{
-        type:String,
-        required: true,
-        maxLength: 255,
-        unique: true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
-    color:{
-        type:String, 
-        minLength: 4,
-        maxLength: 7,
-        required: true,
-    },
     bio:{
         type:String, 
         maxLength: 500,
@@ -39,3 +16,13 @@ const playerSchema = new Schema({
 }, {timestamps: true});
 
 module.exports = mongoose.model('Player',playerSchema);
+
+playerSchema.aggregate([{
+    $lookup:{
+        from: "users",
+        localField: "username_id",
+        foreignField: "_id",
+        as: "username_id"
+    }}]);
+    // .exec(function(err, players){
+    // });
