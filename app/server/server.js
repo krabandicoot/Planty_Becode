389fc0a321ada9853f7a35e2a360365afe851256
Khadja <khadjapaux.contact@gmail.com>
2023-01-30 12:16:22 +0100

    added morgan + trying to fix CORS issues

--- app/server/server.js
+++ app/server/server.js
@@ -1,6 +1,8 @@
 require ('dotenv').config();
 const express = require('express');
 const mongoose = require('mongoose');
+const morgan = require('morgan');
+const cors = require('cors');
 
 const userRoutes = require('./routes/user');
 
@@ -11,6 +13,7 @@ const mongoString = process.env.DB_URL;
 
 // --------- connect to DB 
 mongoose.connect(mongoString);
+mongoose.set('strictQuery', true);
 const database = mongoose.connection;
 
 database.on('error', (error) => {
@@ -21,10 +24,26 @@ database.once('connected', () => {
 });
 
 app.use(express.json());
-app.use('/api/user', userRoutes);
+app.use(cors({
+    origin:['http://localhost:8080','http://locahost:5173'],
+    credentials:true
+}));
+
+app.use(function (req, res, next) {
+
+    res.header('Access-Control-Allow-Origin', ['http://localhost:8080', 'http://locahost:5173']);
+    res.header('Access-Control-Allow-Headers', true);
+    res.header('Access-Control-Allow-Credentials', true);
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
+
+app.use(morgan('common'));
 app.get("/", (req, res) => {
     res.json("Welcome to planty application ☘️");
 });
+app.use('/api/user', userRoutes);
+
 //server set on .env
 app.listen(PORT, () => {
     console.log(`Server Started at ${PORT} 🚀`)
