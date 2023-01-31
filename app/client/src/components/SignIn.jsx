1b61760bb6ee8a2f0fc8c26a25251cc9834ecd9f
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2023-01-31 10:47:35 +0100

    cleaning client file

--- app/client/src/components/SignIn.jsx
+++ app/client/src/components/SignIn.jsx
@@ -1,9 +1,9 @@
 import { useRef, useState, useEffect } from "react";
-import { FaEye } from 'react-icons/fa';
+import { FaEye } from "react-icons/fa";
 import useAuth from "../hooks/useAuth";
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 
-import axios from "../api/axios";
+import axios from '../api/axios';
 const SIGNIN_URL = "/api/user/signin";
 
 
