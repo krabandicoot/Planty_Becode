3410309b22ee015face15eddd5306c46eead5378
Da-nae <grosjean.danae@gmail.com>
2023-02-01 14:19:55 +0100

    leaderboard functionnal

--- app/server/controllers/playerController.js
+++ app/server/controllers/playerController.js
@@ -1,22 +1,27 @@
 const Player = require('../models/playerModel')
 
 // Get all info of a player
-const getAccount = async(req,res) => {
-    // const { username } = req.params;
+// const getAccount = async(req,res) => {
+//     // const { username } = req.params;
     
-    const player = await Player.findOne({username: req.params.username}, function(err, username) {
-        if(err){
-            return res.status(404).json({error: 'Username does not exist'});
-        } else {
-            res.status(200).json(player);
-        }});
-}
+//     const player = await Player.findOne({username: req.params.username}, function(err, username) {
+//         if(err){
+//             return res.status(404).json({error: 'Username does not exist'});
+//         } else {
+//             res.status(200).json(player);
+//         }});
+// }
 
 // Get all players in a list
 const getPlayers = async(req,res) => {
+    try {
     const players = await Player.find({}).sort({leafs: -1});
 
     res.status(200).json(players);
+    // console.log(players);
+    } catch(error) {
+        res.status(400).json({error: error.message});
+    }
 }
 
 // Update the player
@@ -26,6 +31,6 @@ const getPlayers = async(req,res) => {
 // Export all the function
 
 module.exports = { 
-    getAccount,
+    // getAccount,
     getPlayers
 };
--- app/server/routes/player.js
+++ app/server/routes/player.js
@@ -1,9 +1,9 @@
 const express = require('express');
-const {getAccount, getPlayers} = require('../controllers/playerController');
+const { getPlayers} = require('../controllers/playerController');
 
 const router = express.Router();
 // GET an account
-router.get('/:username', getAccount);
+// router.get('/user:username', getAccount);
 
 // GET all players
 router.get('/leaderboard', getPlayers);
