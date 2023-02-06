00df7a8acbe6d7b7218bd2ec6fddcc8c615f2222
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2023-02-06 09:35:19 +0100

    improving header

--- app/client/src/components/Header.jsx
+++ app/client/src/components/Header.jsx
@@ -1,10 +1,24 @@
+import Lottie from "lottie-react";
 import { Link } from "react-router-dom";
+import animationData from '../lotties/logo-Planty.json';
+import { AiOutlineUser } from "react-icons/ai";
 export function Header() {
   return (
     <header className="flex">
-      <img />
-      <Link to="/account">Username</Link>
-      <p>Score</p>
+      <div className="flex">
+        <p className="block relative max-w-min">Plant</p>
+        <Lottie className="container__title-logo absolute w-[15px] top-[2px]" animationData={animationData} loop={false} />
+      </div>
+      <div>
+        <p><AiOutlineUser />  </p>
+        <Link to="/account">Username</Link>
+      </div>
+
+      <div className="flex">
+        <img src="../src/images/icon-leaf.png" alt="Leaf score icon" className="w-[10px]" />
+        <p>Score</p>
+      </div>
+
     </header>
   );
 }
