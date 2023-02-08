c0b649b9dc6a2bfb2c8fef3ffe87ada033a31f3f
Da-nae <grosjean.danae@gmail.com>
2023-02-08 12:00:05 +0100

    test cookie

--- app/server/controllers/commentController.js
+++ app/server/controllers/commentController.js
@@ -3,22 +3,25 @@ const jwt = require('jsonwebtoken');
 const Player = require('../models/playerModel');
 const Comment = require('../models/commentModel');
 
-const getUserInfo = async (req, res) => {
-    let cookie = req.cookie;
-    console.log(cookie);
+// const getUserInfo = async (req, res) => {
+//     let cookie = req.cookie["planty"];
+//     console.log(cookie);
+
+//     let cookiePostman = req.headers
 
     // if(!cookie){
     // // Read the token
     // function parseJwt (token) {
     //     return JSON.parse(Buffer.from(token.split('.')[1], 'base64').toString());
     // }}
-};
+// };
 
 // Create a comment
 const createComment = async (req, res) => {
     const comment = new Comment();
     const { username, text } = req.body;
     const { tree_id } = req.params;
+    getUserInfo();
 
     if (!username || !text) {
         return res.json({
