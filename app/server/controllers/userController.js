020263f262b78fbcf59b01f02ffd41fc29dcb25e
lilouMazzarisi <lisa_mazzarisi@hotmail.com>
2023-02-08 14:14:07 +0100

    committing to merge with jade

--- app/server/controllers/userController.js
+++ app/server/controllers/userController.js
@@ -32,11 +32,11 @@ const signInUser = async (req, res) => {
 
 //signup user 
 const signUpUser = async (req, res) => {
-    const { username, email, password } = req.body;
+    const { username, email, password, color } = req.body;
 
     try {
 
-        const user = await User.signup(username, email, password);
+        const user = await User.signup(username, email, password, color);
         //create token
         const signInToken = createToken(user._id);
         res.status(200).json({ email, signInToken });
