987444bcf5a8b18b4b60d5ae49aaa5f0df3e888c
TreshMiralissa <muratet.jade@yahoo.com>
2023-02-13 13:56:46 +0100

    display map

--- app/client/src/components/Map.jsx
+++ app/client/src/components/Map.jsx
@@ -1,15 +1,22 @@
-import { MapContainer, TileLayer, Marker, Popup, useMap} from "react-leaflet";
-import L from "leaflet";
+import { MapContainer, TileLayer, Marker, Popup, useMap } from "react-leaflet";
 import React from "react";
-import "leaflet/dist/leaflet.css";
 import { useState, useEffect } from "react";
 import axios from "../api/axios";
+import "leaflet/dist/leaflet.css";
+//import icon from "../Images/icon.png";
+//import L from "leaflet";
 
 const TREES_URL = "/api/tree/all";
 
 export default function Map() {
   const latitude = 50.6327565;
   const longitude = 5.5686243;
+
+  // const customIcon = new L.Icon({//creating a custom icon to use in Marker
+  //   iconUrl: icon,
+  //   iconSize: [25, 35],
+  //   iconAnchor: [5, 30]
+  // });
 
   function MapView() {
     let map = useMap();
@@ -18,14 +25,14 @@ export default function Map() {
     return null;
   }
 
-  const { trees, setTrees } = useState("");
+  const [trees, setTrees] = useState("");
 
   useEffect(() => {
 
       const getTrees= async () => {
           try {
               const response = await axios.get(TREES_URL);
-              console.log(response.data);
+              //console.log(response.data);
               setTrees(response.data); 
           } catch (err) {
               console.log(err);
@@ -37,33 +44,22 @@ export default function Map() {
   }, []);
 
   return (
-    <div>
-      {trees?.length
-? (
-      <ul>
-          {trees.map((tree) => <li key={id}>{tree?.latitude + tree?.longitude}</li>)}
-      </ul>
-  ) : <p>No players to display</p>
-}
-    
     <MapContainer
       classsName="map"
       center={[latitude, longitude]}
       zoom={14}
       scrollWheelZoom={true}
-      style = {{height: 80 + "vh", width: 100 +"vh"}}
+      style = {{height: 80 + "vh"}}
     >
       <TileLayer
         attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> 
         contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      {/* {trees?.length ?(  */}
-      {/* // <Marker position={trees.map((trees) => {trees?.latitude + trees?.longitude})}>
-      //   <Popup></Popup>
-      // </Marker>): ("")}      */}
-      {/* <MapView /> */}
+      <Marker position={[latitude, longitude]}>
+        <Popup></Popup>
+      </Marker>
+      <MapView />
     </MapContainer>
-    </div>
   );
 }
