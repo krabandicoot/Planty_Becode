7e97ad5490ca38121b794ce4839c91d2f1f6d7e9
Da-nae <grosjean.danae@gmail.com>
2023-02-14 14:37:00 +0100

    add leaf ok

--- app/server/controllers/userController.js
+++ app/server/controllers/userController.js
@@ -11,44 +11,6 @@ const createToken = (_id) => {//id =  is part of the payload of the token
     return jwt.sign({ _id }, TOKEN, { expiresIn: '3d' });//the second is the secret string only know by the server, the third means the user stays logged in for 3 days before the token expires
 }
 
-// functions to regulate the leaf amout
-// Add player trees x Leafs every 15 min
-const addLeafs = async (username) => {
-    const player = await Player.findOne({username: username});
-
-    const leafAmout = player.leafs;
-
-    const pipeline = [
-        {
-            "$match": {
-                "owner": username
-            }
-        }, 
-        {
-            "$count": "treeCount"
-        }
-    ];
-    
-    const treeCount = Tree.aggregate(pipeline).exec();
-    console.log(treeCount);
-
-    const newLeafAmount = Math.floor(leafAmout +  treeCount);
-    return newLeafAmount;
-}
-setTimeout(addLeafs, 900000);
-
-// Take back half of leafs every hour
-const takeLeafs = async (username) => {
-    const player = await Player.findOne({username: username});
-
-    const leafAmout = player.leafs;
-    const newLeafAmout = Math.floor(leafAmout/2);
-
-    return newLeafAmout;
-}
-
-setTimeout(takeLeafs, 3600000);
-
 //login user 
 const signInUser = async (req, res) => {
     const { username, password } = req.body;
@@ -74,10 +36,6 @@ const signUpUser = async (req, res) => {
     try {
 
         const user = await User.signup(username, email, password, color);
-        
-        // leaf count start :
-        addLeafs(username);
-        takeLeafs(username);
 
         //create token
         const signInToken = createToken(user._id);
